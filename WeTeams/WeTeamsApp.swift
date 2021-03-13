6a0099d0380a9d25af161de0649a3ebb7743c41b
Takanori Hoshi <takanori.hoshi@gmail.com>
2021-03-12 17:03:16 -0800

    Initial commit

---
+++ WeTeams/WeTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeTeamsApp.swift
+//  WeTeams
+//
+//  Created by Takanori Hoshi on 2021-03-12.
+//
+
+import SwiftUI
+
+@main
+struct WeTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
